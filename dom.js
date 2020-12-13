7d2f8c63e1647d069f6d8c02886d16f6e919c324
Kiran Anand <liteninkiran@hotmail.com>
2020-12-13 20:17:03 +0000

    Second lesson

--- dom.js
+++ dom.js
@@ -179,16 +179,102 @@
         even[i].style.backgroundColor = '#cccccc';
     }
 */
+    /*-----------------------------------
+    --   Traversing The DOM            --
+    -----------------------------------*/
 
+    // Get the element called "items" (it is the UL)
+    var itemList = document.querySelector('#items');
+/*
+    // Find the parent (the "main" DIV)
+    var parent = itemList.parentNode;
 
+    // Log the parent object
+    console.log(parent);
 
+    // Change "main" DIV background colour
+    parent.style.backgroundColor = '#f4f4f4';
 
+    // Find the grand-parent (the "container" DIV)
+    var grandParent = parent.parentNode;
 
+    // Log the grand-parent object
+    console.log(grandParent);
 
+    // In most cases, parentElement is the same as parentNode.
+    // The only difference comes when a node's parentNode is 
+    // not an element. If so, parentElement is null. Example:
 
+    document.body.parentNode;                               // the <html> element
+    document.body.parentElement;                            // the <html> element
 
+    document.documentElement.parentNode;                    // the document node
+    document.documentElement.parentElement;                 // null
 
+    (document.documentElement.parentNode === document);     // true
+    (document.documentElement.parentElement === document);  // false
 
+    // Child nodes returns a Node List and includes line breaks
+    console.log(itemList.childNodes);
 
+    // Children returns an HTNL Collection and excludes line breaks
+    console.log(itemList.children);
 
+    // Getting the nth child
+    var child = itemList.children[2];
 
+    console.log(child);
+
+    child.style.backgroundColor = 'Yellow';
+
+    // First child... returns first from childNodes (i.e. includes line breaks)
+    console.log(itemList.firstChild);
+
+    // First element child... returns first from children (i.e. excludes line breaks)
+    var fChild = itemList.firstElementChild
+
+    fChild.textContent = 'Hello!';
+
+    console.log(fChild);
+
+    // Last child... returns last from childNodes (i.e. includes line breaks)
+    console.log(itemList.lastChild);
+
+    // Last element child... returns last from children (i.e. excludes line breaks)
+    var lChild = itemList.lastElementChild
+
+    lChild.textContent = 'Goodbye!';
+
+    // Next sibling... includes line breaks
+    console.log(itemList.nextSibling);
+
+    // Next element sibling... excludes line breaks
+    console.log(itemList.nextElementSibling);
+
+    // Previous sibling... includes line breaks
+    console.log(itemList.previousSibling);
+
+    // Previous element sibling... excludes line breaks
+    console.log(itemList.previousElementSibling);
+
+    // Create element
+    var newDiv = document.createElement('div');
+
+    newDiv.className = 'hello';
+    newDiv.id = 'hello-1';
+    newDiv.setAttribute('title', 'Hello Div');
+
+    console.log(newDiv);
+
+    // Create text node
+    var newDivText = document.createTextNode('Hello World');
+
+    newDiv.appendChild(newDivText);
+
+    var container = document.querySelector('header .container');
+    var h1 = document.querySelector('header h1');
+
+    container.insertBefore(newDiv, h1);
+
+    newDiv.style.fontSize = '30px';
+*/
